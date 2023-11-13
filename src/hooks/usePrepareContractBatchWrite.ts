ba37acd311de0c9b34a1e6c466132bc615056531
Blake Duncan <blakecduncan@gmail.com>
2023-11-13 13:44:44 +0300

    chore: add eth send to usePrepareContractBatchWrite

--- src/hooks/usePrepareContractBatchWrite.ts
+++ src/hooks/usePrepareContractBatchWrite.ts
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import { encodeFunctionData, parseAbi } from "viem";
 import { UsePrepareSendUserOperationConfig, usePrepareSendUserOperation } from "./usePrepareSendUserOperation.js";
 import { isStringArray } from "../utilities/isStringArray.js";
+import { UserOperationCallData } from '@alchemy/aa-core'
 
 export type ContractCall = {
     address: string;
@@ -11,14 +12,39 @@ export type ContractCall = {
     value?: bigint
 }
 
-export const usePrepareContractBatchWrite = (config: Omit<UsePrepareSendUserOperationConfig, 'to' | 'data'> & {calls: ContractCall[]}) => {
+export type UserOpCall = {
+    to: UserOperationCallData['target'],
+    data?: UserOperationCallData['data'],
+    value?: UserOperationCallData['value'],
+}
+
+export const usePrepareContractBatchWrite = (config: Omit<UsePrepareSendUserOperationConfig, 'to' | 'data'> & {calls:(ContractCall | UserOpCall)[]}) => {
     const to = useMemo(() => {
         if (!config.enabled) return []
-        return config.calls.map(call => call.address)
+        if (config.calls) {
+            return config.calls.map(call => {
+                if ('data' in call) {
+                    return call.to;
+                }
+                if ('address' in call) {
+                    return call.address;
+                }
+                return ''
+            })
+        }
     }, [config?.calls])
     const data = useMemo(() => {
         if (!config.enabled) return []
-        return config.calls.map(call => encodeFunctionData({abi: isStringArray(call.abi) ? parseAbi(call.abi) : call.abi, functionName: call.functionName, args: call.args}))
+        if (config.calls) return config.calls.map(call => {
+            if ('data' in call && call.data) {
+                return call.data;
+            }
+            if ('address' in call) {
+                return encodeFunctionData({abi: isStringArray(call.abi) ? parseAbi(call.abi) : call.abi, functionName: call.functionName, args: call.args})
+            }
+            return '0x'
+        })
+        return []
     }, [config?.calls])
     const value = useMemo(() => {
         if (!config.enabled) return []
