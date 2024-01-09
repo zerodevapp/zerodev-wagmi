31d27963a78d8c026117ef171d31875ca1650f84
Blake Duncan <blakecduncan@gmail.com>
2024-01-09 11:04:38 -0500

    chore: update sdk versions

--- src/integrations/rainbowkit/wallets/googleWallet.tsx
+++ src/integrations/rainbowkit/wallets/googleWallet.tsx
@@ -3,11 +3,11 @@ import { AbstractWeb3AuthWalletConnectorOptions } from "../../../connectors/Abst
 import { GoogleSocialWalletConnector } from "../../../connectors/GoogleSocialWalletConnector.js";
 import { Wallet } from "./wallet.js";
 
-export function googleWallet({chains, options}: {chains?: Chain[], options: AbstractWeb3AuthWalletConnectorOptions}): Wallet{
+export function googleWallet({chains, options}: {chains?: Chain[], options: AbstractWeb3AuthWalletConnectorOptions}): Wallet {
     return {
         iconBackground: '#fff',
         iconUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png',
+            'https://cdn1.iconfinder.com/data/icons/google-new-logos-1/32/gmail_new_logo-512.png',
         id: 'openlogin_google',
         name: 'Google',
         createConnector: () => {
