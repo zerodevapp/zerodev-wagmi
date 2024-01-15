2d3913cdcdfa1d92b3e6f805a3c8a05912252b12
Blake Duncan <blakecduncan@gmail.com>
2024-01-15 12:29:07 -0500

    chore: update google icon

--- src/integrations/connectkit/supportedSocialConnectors.tsx
+++ src/integrations/connectkit/supportedSocialConnectors.tsx
@@ -4,7 +4,7 @@ export const supportedSocialConnectors = [
     {
         id: 'google', 
         name: 'Google', 
-        logos: { default: <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png'} alt="Google" />},
+        logos: { default: <img src={'https://cdn1.iconfinder.com/data/icons/google-new-logos-1/32/gmail_new_logo-512.png'} alt="Google" />},
         extensionIsInstalled: () => true
     },
     {
